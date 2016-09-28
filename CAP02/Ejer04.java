6e439802cab75c8231cfe94edf6b06c57cfc1eda
MMARGARITARBV <mmargaritarbv@gmail.com>
2016-09-28 20:29:39 +0200

    terminados ejercicios CAP2

---
+++ CAP02/Ejer04.java
@@ -0,0 +1,19 @@
+/**
+ * Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+ * convertir deberá estar almacenada en una variable.
+ * 
+ * @author Margarita Benitez
+ */
+
+public class Ejer04 {
+  public static void main(String[] args) {
+
+    int    euro = 100 ;
+    double total = (euro * 166.38);
+    
+    System.out.println("CONVERSOR DE EUROS A PESETAS \n "  );
+    System.out.println( euro + " Euros es = " + total + " Pesetas");
+  
+  }
+}
+
---
+++ CAP02/Ejer06.java
@@ -0,0 +1,23 @@
+/**
+ * Escribe un programa que calcule el total de una factura a partir de la base
+ * imponible (precio sin IVA). La base imponible estará almacenada en una
+ * variable.
+ * 
+ * @author Margarita Benitez
+ */
+
+public class Ejer06 {
+  public static void main(String[] args) {
+    
+    int baseImponible = 100;
+    double totalIva  = baseImponible * 0.21;
+    double totalFactura = totalIva + baseImponible;
+        
+    System.out.println("  FACTURA MERCADILLO \n "  );
+    System.out.printf("Base Imponible: %-1s%-6s", baseImponible ," Euros\n");
+    System.out.printf("IVA           : %-1s%-6s", totalIva, " Euros\n \n");
+    System.out.printf("TOTAL FACTURA = %-1s%-6s", (int)totalFactura ," Euros"); //casting de double a entero(int)   
+    }
+}
+
+
