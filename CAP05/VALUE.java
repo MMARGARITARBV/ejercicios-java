526e1b589f480f5b932611683c71ba9ba61ff231
MMARGARITARBV <mmargaritarbv@gmail.com>
2016-11-14 22:05:21 +0100

    añadiendo ejemplo con min-value y max-value

--- CAP05/VALUE.java
+++ CAP05/VALUE.java
@@ -1,35 +1,50 @@
 /**
  * Escribe un programa que lea una lista de diez números y determine cuántos
- * son positivos, y cuántos son negativos. 
+ * son positivos, y cuántos son negativos. Además que nos diga cual es el número 
+ * máximo introducido y cual es el mínimo. 
  *
  * @author Margarita B. V
  */
  
-public class Ejer13 {
+public class VALUE {
   public static void main (String[] args){
-    System.out.println("Nuestro programa te pedirá por teclado 10 números diferentes y:");
-    System.out.println("1. Nos dirá cuantos son positivos y cuantos negativos");
-    System.out.println("2. Nos dirá cual es el valor máximo y cual el mínimo");
+    System.out.println("Números positivos, negativos, máximos y mínimos");
     System.out.println("-------------------------------------------");
     
     int contadorPositivos = 0;
     int contadorNegativos = 0;
+    int iteracion = 0;
+    int max = 0;
+    int min = 0;
     int numero = 0;
-    int numeros = 0;
     
     do{
       System.out.print("\nINTRODUCE EL NÚMERO: ");
       numero = Integer.parseInt(System.console().readLine());
+  
+      if(iteracion == 0){ 
+        max = numero;
+        min = numero;
+      } else{
+    
+        if(numero > max){
+          max = numero;
+        } else if(numero < min){
+          min = numero;
+        }
+      }
            
       if (numero > 0){
         contadorPositivos++;
       }else if (numero < 0){
         contadorNegativos++;
       }       
-      numeros++;
-    } while (numeros < 10);
+      iteracion++;
+    } while (iteracion < 10);
     
     System.out.println("\nLa cantidad de numeros introducidos positivos es: " + contadorPositivos);
     System.out.println("\nLa cantidad de numeros introducidos negativos es: " + contadorNegativos);
+    System.out.println("\nEl número máximo es: " + max);
+    System.out.println("\nEl número mínimo negativo es: " + min);
   }
 }
