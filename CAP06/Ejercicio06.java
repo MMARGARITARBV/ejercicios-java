30cb7735c607593d75c9146d2249ea2c946b9efb
MMARGARITARBV <mmargaritarbv@gmail.com>
2016-12-09 19:30:32 +0100

    actualizando

--- CAP06/Ejercicio06.java
+++ CAP06/Ejercicio06.java
@@ -7,24 +7,31 @@ oportunidades quedan y si el número introducido es menor o mayor que el número
  */
 public class Ejercicio06 {
   public static void main(String[] args) {
-    System.out.println("Usted debe adivinar el número que estoy pensando (entre 0 y 100).\nTiene 5 oportunidades.");
     
-    int numeropensado = (int)(Math.random()*101);
+    System.out.println("Usted debe adivinar el número que estoy pensando (entre 0 y 100). \nTiene 5 oportunidades. \n");
+    
+    int numerosecreto = (int)(Math.random()*101); // Almacenamos el número secreto a adivinar
     boolean acierto = false;
-    System.out.println("El numero pensado es: " + numeropensado);
     
-    for(int x = 4; (x >= 0) && (!acierto); x--) {
-      System.out.println("Por favor, introduzca un número: ");
+    System.out.println("El numero pensado es: " + numerosecreto); // muestra el número secreto y nos sirve para comprobar
+    
+    for(int x = 4; (x >= 0) && (!acierto); x--) { // especificamos en un bucle el número de intentos y si cumple con la la condicion 
+      
+      System.out.print("Por favor, introduzca el número que estoy pensando:"); // introducimos el número 
       int numeroIntroducido = Integer.parseInt(System.console().readLine());
-      if (numeroIntroducido == numeropensado) {
+      System.out.println("");
+      
+      if (numeroIntroducido == numerosecreto) { // si la condicion es verdadera
         acierto = true;
         System.out.println("Enhorabuena, has acertado!");
-      } else if (numeroIntroducido > numeropensado) {
+        
+      } else if (numeroIntroducido > numerosecreto) { // si numero introducido es mayor que el número secreto
         System.out.println("El número que estoy pensando es menor al número que has introducido");
-        System.out.println("Te quedan " + x + " oportunidades.");
-      } else if (numeroIntroducido < numeropensado) {
+        System.out.println("Te quedan " + x + " oportunidades.\n"); // nos dice las oportunidades que le quedan
+        
+      } else if (numeroIntroducido < numerosecreto) { // nos dice que el número introducido es mayor al número secreto
         System.out.println("El número que estoy pensado es mayor al número que has introducido");
-        System.out.println("Te quedan " + x + " oportunidades.");
+        System.out.println("Te quedan " + x + " oportunidades.\n"); // nos dice las oportunidades que te quedan
       }
     }
   }
