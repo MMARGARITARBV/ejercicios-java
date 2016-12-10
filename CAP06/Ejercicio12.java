204e6a2fc12f9311e61e6d5f81e5efbada0e9c9c
MMARGARITARBV <mmargaritarbv@gmail.com>
2016-12-10 18:31:44 +0100

    actualizando

--- CAP06/Ejercicio12.java
+++ CAP06/Ejercicio12.java
@@ -6,13 +6,15 @@ carácter.
  * @author Margarita B.V
  */
 public class Ejercicio12 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException { // Throws nos permite utilizar el thread.sleep(1)
     System.out.println("Este programa llena la pantalla de caracteres.");
     int x = 1;
+    String celeste = "\033[36m"; // pintamos los caracteres de color celeste
     
     while (x > 0) {
-      char caracter = (char)((Math.random() * 95) + 32);
-      System.out.print(caracter);
+      char caracter = (char)((Math.random() * 95) + 32); // char entre 32 y 126
+      Thread.sleep(1);//interrupcion de 1 milisegundo para darle similitud con Matrix
+      System.out.print(celeste + caracter);
       }
   }
 }
--- CAP06/Ejercicio13.java
+++ CAP06/Ejercicio13.java
@@ -10,13 +10,15 @@ public class Ejercicio13 {
     System.out.println("Este programa simula la tirada de dos dados.");
     int dadouno = 0;
     int dadodos = 0;
+    
     int x = 1;
     
+    // Generamos la tirada de dos dados hasta que ambos tengan el mismo valor
     do {
       dadouno = (int)(Math.random() * 6 ) + 1;
       dadodos = (int)(Math.random() * 6 ) + 1;
-      System.out.println("La tirada nº" + x + " del primer dado y el segundo es: " + dadouno + " " + dadodos);
-      x++;
-    } while (dadouno != dadodos);
+      System.out.println("La tirada nº" + x + " del primer dado es: " + dadouno + " y el segundo es: " + dadodos);
+      x++; // vuelve a hacer el bucle hasta que cumpla con la condición de que los dos dados tengan el mismo valor
+    } while (dadouno != dadodos); // mietras que dado 1 y dado dos tengan el mismo valor
   }
 }
